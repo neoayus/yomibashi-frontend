67d1238bf0cd1ad7ed896fd63b6c71ce390e4291
neoayus <ayus43@outlook.com>
2026-03-01 21:38:02 +0530

    feat: upload file to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
+
 export default function App(){
+  const [file, setFile] = useState(null);
+
+  function handleChange(e){
+    const file = e.target.files[0];
+    setFile(file);
+  }
+  
+  const handleUpload = async() => {
+
+    console.log("haneling upload...");
+
+    const formData = new FormData(); 
+    formData.append("file", file);
+    
+    const res = await fetch("http://127.0.0.1:8000/upload", {
+      method: "POST", 
+      body: formData,
+    })
+    
+    const data = await res.json(); 
+    console.log(data);
+  }
+
   return(
     <>
-      this that app.
+      <h2>KA-ROM : Kanji 2 Romaji Subtitle Converter </h2>
+      <input type="file" onChange={handleChange}/>
+      <button onClick={handleUpload}>Upload</button>
     </>
   )
 }
